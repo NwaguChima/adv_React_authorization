a0faa6f3897fac0ce18a1338e507def8618676ec
nwagu chima <chima.nwagu@decagon.dev>
2022-03-23 08:53:10 +0100

    initial setup and commit

---
+++ src/components/Register/Register.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useRef, useState, useEffect } from "react";
+// import {} from ''
+
+const Register = () => {
+  return <div>Register</div>;
+};
+
+export default Register;
