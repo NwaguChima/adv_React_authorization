aeb8e482e69760885c7cc21ec4d04f46c5b70a37
nwagu chima <chima.nwagu@decagon.dev>
2022-03-25 00:37:25 +0100

    setting up dummy pages to implement route protection and authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
 import React from "react";
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Layout from "./components/Others/Layout";
+import LinkPage from "./components/Others/LinkPage";
+import Unauthorized from "./components/Others/Unauthorized";
+import Editor from "./components/Others/Editor";
+import Admin from "./components/Others/Admin";
+import Lounge from "./components/Others/Lounge";
+import Home from "./components/Others/Home";
+import Missing from "./components/Others/Missing";
 
 function App() {
   return (
-    <main>
-      <h1 className="header">Welcome! Kindly Register to Begin...</h1>
-      <div className="form_container">
-        {/* <Register /> */}
-        <Login />
-      </div>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Public routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="linkpage" element={<LinkPage />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+
+        {/* We want to protect these routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="editor" element={<Editor />} />
+        <Route path="admin" element={<Admin />} />
+        <Route path="lounge" element={<Lounge />} />
+
+        {/* catch all */}
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
